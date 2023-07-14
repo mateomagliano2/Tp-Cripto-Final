727c1d6ef804aa853f2ea35bb8933eb6afedfb71
Mateo Magliano <mmagliano@ddvs.edu.ar>
2023-07-14 20:19:36 -0300

    commit uno

---
+++ src/views/HistorialView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h2>Historial de transacciones</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Criptomoneda</th>
+          <th>Cantidad</th>
+          <th>Fecha y hora</th>
+          <th>Precio</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <!-- <tbody>
+         <tr v-for="transaction in transactions" :key="transaction._id">
+          <td>{{ transaction.cryptocurrency }}</td>
+          <td>{{ transaction.amount }}</td>
+          <td>{{ transaction.date }}</td>
+          <td>{{ transaction.price }}</td>
+          <td> -->
+            <!-- <button @click="editTransaction(transaction)">Editar</button>
+            <button @click="deleteTransaction(transaction)">Borrar</button>
+          </td>
+        </tr>
+      </tbody> -->
+    </table>
+  </div>
+</template>
+
+<script>
+//export default {
+//  data() {
+//    return {
+//      transactions: []
+//    }
+//  },
+//  created() {
+//    fetch(`https://laboratorio3-f36a.restdb.io/rest/transactions?q={"user_id": "${this.$store.state.userId}"}`, {
+//      headers: {
+//        'Content-Type': 'application/json',
+//        'x-apikey': 'TU_API_KEY' // Reemplazar con tu API Key
+//      }
+//    })
+//      .then(response => response.json())
+//      .then(data => {
+//        this.transactions = data
+//      })
+//  },
+//  methods: {
+//    editTransaction(transaction) {
+//      Implementar la lógica para editar una transacción
+//    },
+//    deleteTransaction(transaction) {
+//      Implementar la lógica para borrar una transacción
+//    }
+//  }
+//}
+</script>
---
+++ src/views/NombreUsuario.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <h2>Ingresar Nombre de Usuario</h2>
+      <form @submit="submitForm">
+        <label for="username">Nombre de usuario:</label>
+        <input
+          id="username"
+          type="text"
+          v-model="username"
+          :maxlength="8"
+          pattern="[a-zA-Z]+"
+          required
+        />
+        <button type="submit">Guardar</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+      };
+    },
+    methods: {
+      submitForm(event) {
+        event.preventDefault();
+        // Validar el nombre de usuario
+        if (/[^0-9a-zA-Z]/.test(this.username)) {
+          alert('El nombre de usuario no puede contener números ni caracteres especiales.');
+          return;
+        }
+        if (this.username.length > 8) {
+          alert('El nombre de usuario debe tener máximo 8 caracteres.');
+          return;
+        }
+        // Guardar el nombre de usuario para utilizar en otras vistas
+        this.$store.commit('setUsername', this.username);
+        // Redirigir a otra vista
+        this.$router.push('/otra-vista');
+      },
+    },
+  };
+  </script>
